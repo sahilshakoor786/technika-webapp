940354b749f3d2a0a1eed9a9eb2e5829db36915a
Tejpratap <tejpratap.singh@trell.in>
2022-04-21 17:08:37 +0530

    google auth url api

---
+++ pages/api/auth/google/url.js
@@ -0,0 +1,19 @@
+const querystring = require("querystring");
+export default function handler(req, res) {
+  const rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
+  const options = {
+    redirect_uri: `${process.env.SERVER_ROOT_URI}/api/auth/google/callback`,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    access_type: "offline",
+    response_type: "code",
+    prompt: "consent",
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ].join(" "),
+  };
+
+  const url = `${rootUrl}?${querystring.stringify(options)}`;
+
+  res.status(200).json({ url });
+}
