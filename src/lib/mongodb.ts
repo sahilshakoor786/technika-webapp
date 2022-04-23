0f557ce886fca0fbd0b490bae6c235d00783445b
Tejpratap <tejpratap.singh@trell.in>
2022-04-23 20:40:20 +0530

    add mongo db

---
+++ src/lib/mongodb.ts
@@ -0,0 +1,54 @@
+import { MongoClient, Db, MongoClientOptions } from "mongodb";
+import mongoose from "mongoose";
+const MONGODB_URI: string | undefined = process.env.MONGODB_URI;
+const MONGODB_DB = process.env.DB_NAME;
+
+// check the MongoDB URI
+if (!MONGODB_URI) {
+  throw new Error("Define the MONGODB_URI environmental variable");
+}
+
+// check the MongoDB DB
+if (!MONGODB_DB) {
+  throw new Error("Define the MONGODB_DB environmental variable");
+}
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+let client: MongoClient;
+let db: Db;
+
+export async function connectToDatabase(): Promise<{
+  client: MongoClient;
+  db: Db;
+}> {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      bufferCommands: false,
+      bufferMaxEntries: 0,
+      useFindAndModify: true,
+      useCreateIndex: true,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
