2b1c4a9612c9cf293a324d017f2527a1c2d7aa55
Tejpratap <tejpratap.singh@trell.in>
2022-04-23 21:31:33 +0530

    video encoding and hls h.264

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,34 +1,51 @@
 import PrimaryButton from "../PrimaryButton";
+import Hls from "hls.js";
+import React from "react";
+export default class VideoPlayer extends React.Component {
+  state = {};
+  componentDidMount() {
+    if (Hls.isSupported() && this.player) {
+      const video = this.player;
+      const hls = new Hls();
+      const url = "/videos/main_video/720p.m3u8";
 
-export default function HeroSection() {
-  return (
-    <section
-      id="hero"
-      className="relative flex items-center justify-center h-screen overflow-hidden"
-    >
-      <video
-        src="/videos/hero.mp4"
-        className="absolute w-auto min-w-full min-h-full max-w-none"
-        autoPlay
-        loop
-        muted
-      />
-
-      <div
-        className="z-10 relative text-white md:w-1/2 
-      bg-blue-800/10 backdrop-blur flex flex-col justify-center shadow-lg py-6 px-2 space-y-2"
+      hls.loadSource(url);
+      hls.attachMedia(video);
+      hls.on(Hls.Events.MANIFEST_PARSED, function () {
+        video.play();
+      });
+    }
+  }
+  render() {
+    return (
+      <section
+        id="hero"
+        className="relative flex items-center justify-center h-screen overflow-hidden"
       >
-        <span className="text-6xl md:text-8xl text-white font-dm-sans text-center break-all">
-          #tecknika
-        </span>
-        <span className="text-5xl md:text-6xl text-white font-dm-sans text-center">
-          Go beyond
-        </span>
+        <video
+          className="absolute w-auto min-w-full min-h-full max-w-none"
+          ref={(player) => (this.player = player)}
+          autoPlay={true}
+          loop
+          muted
+        />
 
-        <span className="flex justify-center">
-          <PrimaryButton text="Register now" onClick={() => alert("red")} />
-        </span>
-      </div>
-    </section>
-  );
+        <div
+          className="z-10 relative text-white md:w-1/2 
+      bg-blue-800/10 backdrop-blur flex flex-col justify-center shadow-lg py-6 px-2 space-y-2"
+        >
+          <span className="text-6xl md:text-8xl text-white font-dm-sans text-center break-all">
+            #tecknika
+          </span>
+          <span className="text-5xl md:text-6xl text-white font-dm-sans text-center">
+            Go beyond
+          </span>
+
+          <span className="flex justify-center">
+            <PrimaryButton text="Register now" onClick={() => alert("red")} />
+          </span>
+        </div>
+      </section>
+    );
+  }
 }
