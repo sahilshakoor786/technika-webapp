7055582cf11312fbac5539bb3fd8514db81cc67b
Tejpratap <tejpratap.singh@trell.in>
2022-04-29 00:06:43 +0530

    update url

--- frontend/src/pages/register/complete.tsx
+++ frontend/src/pages/register/complete.tsx
@@ -147,7 +147,7 @@ export default function RegisterPage() {
 
     try {
       const res = await axiosInstance.put(
-        `/profile?id=${token?.id}`,
+        `/profile/${token?.id}`,
         {
           name: nameInput.value,
           phone: phoneInput.value,
