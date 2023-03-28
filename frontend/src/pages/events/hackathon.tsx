7b54b7c76a283d848088a5eb06d2fcf53cd23585
Sahil Shakoor <98870741+sahilshakoor786@users.noreply.github.com>
2023-03-29 00:36:11 +0530

    Add files via upload

---
+++ frontend/src/pages/events/hackathon.tsx
@@ -0,0 +1,147 @@
+import gsap from "gsap";
+
+import { useState } from "react";
+import Auth from "src/components/Auth";
+import EventRegistrationForm from "src/components/EventRegistrationForm";
+import Layout from "src/components/Layout";
+import SecondaryButton from "src/components/SecondaryButton";
+import Prize from "src/components/Prize";
+
+export default function Page() {
+  const [popup, setPopup] = useState(false);
+
+  function handleEventFormPopup() {
+    if (!popup) {
+      gsap.to("#event-popup", {
+        top: 0,
+        opacity: 1,
+        scale: 1,
+        duration: 0.5,
+        ease: "slow(0.7, 0.7, false)",
+      });
+    } else {
+      gsap.to("#event-popup", {
+        top: "100%",
+        opacity: 0,
+        scale: 0.5,
+        duration: 0.5,
+        ease: "slow(0.7, 0.7, false)",
+      });
+    }
+    setPopup(!popup);
+  }
+
+  return (
+    // <Auth>
+    <Layout>
+      <>
+        <main
+          className="h-full min-h-screen overflow-x-hidden 
+          flex justify-center items-center"
+        >
+          <div
+            className="p-10 relative text-white md:w-3/4  h-1/2
+            bg-blue-800/10 backdrop-blur flex flex-col justify-center
+              shadow-lg py-6 px-2 space-y-2 rounded-lg place-items-center"
+          >
+            <h1
+              className="font-primary text-3xl md:text-5xl text-center 
+                mt-32 md:mt-20 mb-10"
+            >
+              HACKATHON
+            </h1>
+            <Prize prize="1000" />
+
+            <div
+              className="w-full grid grid-cols-1 lg:grid-cols-3 
+                gap-y-10 place-items-center bg-white/50 py-10 rounded-xl bg-cover bg-fixed"
+              style={{
+                height: 500,
+                backgroundImage: `url("https://d2jf5yk8vvx0ti.cloudfront.net/images/rasterize.avif")`,
+              }}
+            >
+              <span className="col-span-2"></span>
+
+              <div
+                className="rounded-lg grid grid-cols-2 gap-2 p-10 
+                 backdrop-blur shadow-lg bg-white/30"
+              >
+                <span className="font-primary font-bold text-slate-800 ">
+                  Venue
+                </span>
+                <span className="text-slate-800 ">CSE Lab
+</span>
+                <span className="font-primary font-bold text-slate-800 ">
+                  Date
+                </span>
+                <span className="text-slate-800">12 Apr</span>
+
+                <span className="font-primary font-bold text-slate-800 ">
+                  Time
+                </span>
+                <span className="text-slate-800">12 - 4 PM
+</span>
+              </div>
+            </div>
+
+            <div className="mb-10">
+              <SecondaryButton
+                text="Register to event"
+                onClick={handleEventFormPopup}
+              />
+            </div>
+
+            <div
+              className="overflow-hidden font-sans text-xl 
+              text-white text-center max-w-xl"
+            >
+              <h1 className="font-primary text-2xl md:text-5xl text-center mt-5 mb-5">
+                About the Event
+                <br />
+              </h1>
+              Welcome to the hackathon event! In this engaging event, you will have the opportunity to collaborate with other tech enthusiasts to create innovative software projects. Whether you are a seasoned developer or a newcomer to the field, this event is an opportunity to flex your creativity, problem-solving skills, and teamwork abilities. You will have access to mentors, resources, and support to help you bring your ideas to life. Throughout the event, you will work tirelessly to prototype your projects and compete for prizes. But more importantly, you will have the opportunity to learn new skills, network with other professionals, and contribute to the development of new technologies that can make a real impact in the world. So let's get ready to hack!
+              <h1 className="font-primary text-2xl md:text-5xl text-center mt-5 mb-5">
+                Guidelines: <br />
+              </h1>
+              In this event, participants will get an opportunity to show off
+              their front-end web design skills (HTML/CSS/JAVASCRIPT). All the
+              participants will be shown a web design for a brief time and will
+              be provided all relevant data (dimensions, color codes, etc). They
+              have to replicate the original web design. A participant whose web
+              design is closest to the original web design shown in the
+              beginning will be considered winner.
+              <h1 className="font-primary text-2xl md:text-5xl text-center mt-5 mb-5">
+                Coordinators: <br />
+              </h1>
+              Sumit (3rd btech IT) : 9568243301
+              <br />
+            </div>
+          </div>
+        </main>
+
+        <div
+          id="event-popup"
+          className="fixed w-screen h-screen  top-full left-0 flex justify-center items-center 
+            backdrop-blur opacity-0 px-5 z-40 scale-50"
+        >
+          <div className="w-full max-w-xl h-97/100 bg-white shadow-lg relative rounded-lg">
+            <button
+              onClick={handleEventFormPopup}
+              className="transition ease-in-out delay-15 z-20 -right-4 -top-4 
+              absolute rounded-full w-12 h-12 bg-pink-500 
+              shadow-lg grid place-items-center hover:scale-110"
+            >
+              <img
+                src="https://d2jf5yk8vvx0ti.cloudfront.net/images/close.svg"
+                className="w-6 h-6"
+              />
+            </button>
+
+            <EventRegistrationForm eventId="TSCGC03" />
+          </div>
+        </div>
+      </>
+    </Layout>
+    // </Auth>
+  );
+}
