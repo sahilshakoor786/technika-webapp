11de6226c799ac3079599fbb31b6158666e30e1a
Sahil Shakoor <98870741+sahilshakoor786@users.noreply.github.com>
2023-03-30 23:08:05 +0530

    Added photography team

--- frontend/src/pages/team.tsx
+++ frontend/src/pages/team.tsx
@@ -145,6 +145,31 @@ export default function EventsPage() {
             ))}
           </div>
 
+           <h1
+            className="font-primary text-3xl md:text-5xl text-center 
+                mt-32 md:mt-20 mb-10"
+          >
+            Photography Team{}
+          </h1>
+
+          <div className="flex justify-center items-center gap-y-8 gap-x-8 flex-wrap">
+            {photographyteam.map((member) => (
+              <TeamImage
+                key={member?.Timestamp}
+                src={member?.Photo}
+                width={350}
+                height={400}
+                name={member?.Name}
+                position={member?.["Position in TSC"]}
+                instagram={member?.["Instagram profile URL"]}
+                facebook="https://www.facebook.com/technicalsubcouncihbtu/"
+                linkedin={member?.["LinkedIn profile URL "]}
+              />
+            ))}
+          </div>
+
+
+          
         </div>
       </main>
     </Layout>
