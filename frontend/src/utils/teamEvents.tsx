6e4136a4c6c31fd64dfb5d53b03360d93585ee45
utsav82 <utsa.yadav.94@gmail.com>
2023-04-01 18:19:58 +0530

    Added events team

---
+++ frontend/src/utils/teamEvents.tsx
@@ -0,0 +1,90 @@
+export const teamEvents = [
+    {
+    Timestamp: "3/28/2023 21:00:45",
+    Name: "Samarthya",
+    Photo: "https://drive.google.com/uc?id=1iiLN_bGZ9patHMsSe62lPPMUUJgfMigu",
+    "LinkedIn profile URL ":
+      "https://www.linkedin.com/in/samarthya-choudhary-1a3654229",
+    "Instagram profile URL":
+      "https://instagram.com/ii_.jaat._ii?igshid=YmMyMTA2M2Y=",
+    "Position in TSC": "Events Head and Logistics Head",
+  },
+  {
+    Timestamp: "3/28/2023 22:55:37",
+    Name: "Divyansh Vaibhav",
+    Photo: "https://drive.google.com/uc?id=1EV7gaBVIvsSSOqxjgGu0hO_tkFKI6YaM",
+    "LinkedIn profile URL ":
+      "https://www.linkedin.com/in/divyansh-vaibhav-b49206222",
+    "Instagram profile URL": "https://www.instagram.com/divyansh__vaibhav/",
+    "Position in TSC": "Events Head",
+  },
+  {
+    Timestamp: "3/29/2023 1:11:38",
+    Name: "Haza Rahman ",
+    Photo: "https://drive.google.com/uc?id=1XTv1eUQTFFrPmozpitg-M6kVMTDz4ITE",
+    "LinkedIn profile URL ":
+      "https://www.linkedin.com/in/haza-rahman-b5a7b7243",
+    "Instagram profile URL":
+      "https://instagram.com/rahman_haza?igshid=ZDdkNTZiNTM=",
+    "Position in TSC": "Associate Event Head",
+  },
+   {
+    Timestamp: "3/29/2023 1:17:29",
+    Name: "Utkarsh Vashishtha",
+    Photo: "https://drive.google.com/uc?id=15Ste1Z5HKGvh834PM_636jIt-X6BzmbU",
+    "LinkedIn profile URL ":
+      "https://www.linkedin.com/in/utkarsh-vashishtha-24227a228/",
+    "Instagram profile URL": "https://www.instagram.com/utkar_sh3038/",
+    "Position in TSC": "Events ",
+  },
+  {
+    Timestamp: "3/29/2023 1:24:46",
+    Name: "Rohit Gupta ",
+    Photo: "https://drive.google.com/uc?id=1YhoVXjc2SiK1TKyEAdY7VoqyMjKcJFGz",
+    "LinkedIn profile URL ":
+      "https://www.linkedin.com/in/rohit-gupta-09389a1a7",
+    "Instagram profile URL":
+      "https://instagram.com/rohitgupta_7700?igshid=ZDdkNTZiNTM=",
+    "Position in TSC": "Designing,Marketing,Publicity,Event management ",
+  },
+  {
+    Timestamp: "3/29/2023 13:22:11",
+    Name: "Utkarsh Kaushik ",
+    Photo: "https://drive.google.com/uc?id=1MGl7uAYdKJXFt1OxRcjqSeq-QjZm0V99",
+    "LinkedIn profile URL ":
+      "https://www.linkedin.com/in/utkarsh-kaushik-22a7b2223",
+    "Instagram profile URL":
+      "https://instagram.com/utkarsh.kaushikk_?igshid=ZDdkNTZiNTM=",
+    "Position in TSC": "Associate head of events department ",
+  },
+  {
+    Timestamp: "3/29/2023 13:42:04",
+    Name: "Dheerendra Pratap Singh ",
+    Photo: "https://drive.google.com/uc?id=1vV1M2l1RQqMuk4c8fTxh42tGUn2DbWNo",
+    "LinkedIn profile URL ":
+      "https://www.linkedin.com/in/dheerendra-pratap-singh-0ba285228",
+    "Instagram profile URL":
+      "https://instagram.com/its__dpsingh?igshid=ZDdkNTZiNTM=",
+    "Position in TSC": "Events",
+  },
+  {
+    Timestamp: "3/29/2023 15:49:52",
+    Name: "Kashish Azhar",
+    Photo: "https://drive.google.com/uc?id=1vY3aZs18ugjYhp9eGCrox-f2sPMcCcxd",
+    "LinkedIn profile URL ":
+      "https://www.linkedin.com/in/kashish-azhar-043269264",
+    "Instagram profile URL":
+      "https://instagram.com/kashish_azhar?igshid=YmMyMTA2M2Y=",
+    "Position in TSC": "Events team",
+  },
+  {
+    Timestamp: "3/31/2023 10:18:24",
+    Name: "Harsh Kalra ",
+    Photo: "https://drive.google.com/uc?id=1lB5hu_8-jDfUH80IhKBppvaLKPS4ucDe",
+    "LinkedIn profile URL ": "https://www.linkedin.com/in/harsh-k-7723101b2",
+    "Instagram profile URL":
+      "https://instagram.com/harsh_kalra_2003?igshid=ZDdkNTZiNTM=",
+    "Position in TSC": "Events Associate",
+  },
+  ];
+  
