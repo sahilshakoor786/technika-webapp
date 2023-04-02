91d3b138936252a2b38be44a3d403bfe351e6654
Raunak Agarwal <raunakagarwal173@gmail.com>
2023-04-02 16:17:37 +0530

    pr team made

--- frontend/src/pages/team.tsx
+++ frontend/src/pages/team.tsx
@@ -11,6 +11,8 @@ import { photographyteam } from "src/utils/photographyteam";
 import { teamPublicity } from "src/utils/teamPublicity";
 import { teamSecurity } from "src/utils/teamSecurity";
 import { teamPR } from "src/utils/teamPR";
+import { teamContent } from "src/utils/teamContent";
+import { teamEvents } from "src/utils/teamEvents";
 
 export default function EventsPage() {
   return (
@@ -234,6 +236,77 @@ export default function EventsPage() {
               />
             ))}
           </div>
+        </div>
+
+        {/* Events Team */}
+        <h1
+          className="font-primary text-3xl md:text-5xl text-center 
+                mt-32 md:mt-20 mb-10"
+        >
+          Events Team
+        </h1>
+
+        <div className="flex justify-center items-center gap-y-8 gap-x-8 flex-wrap py-20">
+          {teamEvents.map((member) => (
+            <TeamImage
+              key={member?.Timestamp}
+              src={member?.Photo}
+              width={350}
+              height={400}
+              name={member?.Name}
+              position={member?.["Position in TSC"]}
+              instagram={member?.["Instagram profile URL"]}
+              facebook="https://www.facebook.com/technicalsubcouncihbtu/"
+              linkedin={member?.["LinkedIn profile URL "]}
+            />
+          ))}
+        </div>
+
+        {/* Content Team */}
+        <h1
+          className="font-primary text-3xl md:text-5xl text-center 
+                mt-32 md:mt-20 mb-10"
+        >
+          Content Team
+        </h1>
+
+        <div className="flex justify-center items-center gap-y-8 gap-x-8 flex-wrap py-20">
+          {teamContent.map((member) => (
+            <TeamImage
+              key={member?.Timestamp}
+              src={member?.Photo}
+              width={350}
+              height={400}
+              name={member?.Name}
+              position={member?.["Position in TSC"]}
+              instagram={member?.["Instagram profile URL"]}
+              facebook="https://www.facebook.com/technicalsubcouncihbtu/"
+              linkedin={member?.["LinkedIn profile URL "]}
+            />
+          ))}
+        </div>
+
+        <h1
+          className="font-primary text-3xl md:text-5xl text-center 
+                mt-32 md:mt-20 mb-10"
+        >
+          Public and Relations Team
+        </h1>
+
+        <div className="flex justify-center items-center gap-y-8 gap-x-8 flex-wrap py-20">
+          {teamPR.map((member) => (
+            <TeamImage
+              key={member?.Timestamp}
+              src={member?.Photo}
+              width={350}
+              height={400}
+              name={member?.Name}
+              position={member?.["Position in TSC"]}
+              instagram={member?.["Instagram profile URL"]}
+              facebook="https://www.facebook.com/technicalsubcouncihbtu/"
+              linkedin={member?.["LinkedIn profile URL "]}
+            />
+          ))}
         </div>
       </main>
     </Layout>
