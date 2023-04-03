f8020d394aa5f0db21f01f07127849621362caa9
utsav82 <utsa.yadav.94@gmail.com>
2023-04-03 18:21:43 +0530

    Updated 2 images in team

--- frontend/src/utils/teamPR.tsx
+++ frontend/src/utils/teamPR.tsx
@@ -9,7 +9,7 @@ export const teamPR = [
   {
     Timestamp: "3/29/2023 8:30:34",
     Name: "SARTHAK SINGH ",
-    Photo: "https://drive.google.com/uc?id=1w_OaO9Bud41zt28UO9GxLQsvIsqM1x6L",
+    Photo: "https://drive.google.com/uc?id=1WaQv8VG1gCOBfaOV0SKNTMlxSi0A9sYc",
     "Instagram profile URL": "sarthaksingh2045",
     "Position in TSC":
       "Associate head  public relation  , associate head hospitality ",
@@ -37,7 +37,7 @@ export const teamPR = [
   {
     Timestamp: "3/29/2023 13:27:30",
     Name: "Vivek Kumar",
-    Photo: "https://drive.google.com/uc?id=1Sn0g-DGxcs9aF66y4vtoHM6QmzrbLnbP",
+    Photo: "https://drive.google.com/uc?id=164vVTbzD6WKKxUUUn5VLwL2Hj4Ek3iVU",
     "LinkedIn profile URL ":
       "https://www.linkedin.com/in/vivek-kumar-193a17233",
     "Instagram profile URL": "__.vivek__kumar",
