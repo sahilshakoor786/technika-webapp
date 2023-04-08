5ff34e184971e0d3a7f8b2f81c707a659aace9a1
Raunak Agarwal <71973645+Raunak173@users.noreply.github.com>
2023-04-08 13:51:47 +0530

    Update team.tsx

--- frontend/src/pages/team.tsx
+++ frontend/src/pages/team.tsx
@@ -59,7 +59,7 @@ export default function EventsPage() {
           <div className="flex justify-center items-center gap-y-8 gap-x-8 flex-wrap py-20">
             <TeamImage
               key={"Devendra Singh Parihar"}
-              src="https://drive.google.com/file/d/1oU8x2dWlhcpPYb1PVC2vMA-zPfvAwBBd/view?usp=sharing"
+              src="https://drive.google.com/file/d/1oU8x2dWlhcpPYb1PVC2vMA-zPfvAwBBd/view"
               width={300}
               height={400}
               name={"Devendra Singh Parihar"}
