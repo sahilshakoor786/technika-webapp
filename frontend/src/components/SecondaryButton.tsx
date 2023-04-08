7f2756cab61b3b64da034caafbdb40f007b95bab
Raunak Agarwal <71973645+Raunak173@users.noreply.github.com>
2023-04-08 23:50:48 +0530

    Update SecondaryButton.tsx

--- frontend/src/components/SecondaryButton.tsx
+++ frontend/src/components/SecondaryButton.tsx
@@ -1,6 +1,7 @@
 type PrimaryButtonProps = {
     text: string;
     onClick?: () => void;
+    form_link: any;
   };
   
   export default function SecondaryButton({ text, onClick,form_link }: PrimaryButtonProps) {
