e395d1b3c12dae01e8a52c10f19a9ce66f89f712
Sahil Shakoor <98870741+sahilshakoor786@users.noreply.github.com>
2023-04-08 16:32:02 +0530

    Added coordinator and logistics team

---
+++ frontend/src/utils/logistics.tsx
@@ -0,0 +1,17 @@
+export const logistics=[  {
+    Timestamp: "3/28/2023 21:00:45",
+    Name: "Samarthya",
+    Photo: "https://drive.google.com/uc?id=1iiLN_bGZ9patHMsSe62lPPMUUJgfMigu",
+    "LinkedIn profile URL ":
+      "https://www.linkedin.com/in/samarthya-choudhary-1a3654229",
+    "Instagram profile URL":
+      "https://instagram.com/ii_.jaat._ii?igshid=YmMyMTA2M2Y=",
+    "Position in TSC": "Events Head and Logistics Head",
+  }, {
+    Timestamp: "3/29/2023 1:22:50",
+    Name: "Abhyudaya pandey",
+    Photo: "https://drive.google.com/uc?id=1aGWh8yWePVRHQWCHUEm0xVKmU7GhY_FS",
+    "Instagram profile URL":
+      "https://instagram.com/abhyudaya______?igshid=ZDdkNTZiNTM=",
+    "Position in TSC": "Associate logistics head and associate security head",
+  }]
