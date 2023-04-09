70afe1162b942c9b2048b26ab93d9ef739551e88
Raunak Agarwal <raunakagarwal173@gmail.com>
2023-04-09 13:30:34 +0530

    register form for all events removed

--- frontend/src/components/EventRegistrationForm.tsx
+++ frontend/src/components/EventRegistrationForm.tsx
@@ -209,166 +209,167 @@ export default function EventRegistrationForm({
   };
 
   return (
-    <div className="h-full flex flex-col items-center justify-center overflow-y-scroll">
-      {loading ? (
-        <Spinner />
-      ) : error ? (
-        <>
-          <h1 className="font-primary  m-5  p-5 text-xl text-center">
-            An Unexpected error occured{" "}
-          </h1>
-          <p>{error}</p>
-        </>
-      ) : isClosed ? (
-        <>
-          <h1 className="font-primary text-xl text-center m-5 p-5">
-            Online Registration is closed , You can register for offline .
-            Please contact the event coordinators.
-          </h1>
-        </>
-      ) : (
-        <>
-          {registered ? (
-            <span className="font-primary text-xl">Registered to event</span>
-          ) : payment ? (
-            <>
-              <h1 className="font-primary text-3xl text-center font-bold">
-                Register to {event?.eventName}
-              </h1>
+    // <div className="h-full flex flex-col items-center justify-center overflow-y-scroll">
+    //   {loading ? (
+    //     <Spinner />
+    //   ) : error ? (
+    //     <>
+    //       <h1 className="font-primary  m-5  p-5 text-xl text-center">
+    //         An Unexpected error occured{" "}
+    //       </h1>
+    //       <p>{error}</p>
+    //     </>
+    //   ) : isClosed ? (
+    //     <>
+    //       <h1 className="font-primary text-xl text-center m-5 p-5">
+    //         Online Registration is closed , You can register for offline .
+    //         Please contact the event coordinators.
+    //       </h1>
+    //     </>
+    //   ) : (
+    //     <>
+    //       {registered ? (
+    //         <span className="font-primary text-xl">Registered to event</span>
+    //       ) : payment ? (
+    //         <>
+    //           <h1 className="font-primary text-3xl text-center font-bold">
+    //             Register to {event?.eventName}
+    //           </h1>
 
-              <div
-                className="grid grid-cols-2 gap-2 p-5
-            text-slate-600 bg-slate-100 rounded-lg m-5"
-              >
-                <span className="font-primary font-bold text-slate-800">
-                  Solo Event
-                </span>
-                <span>{event?.isSoloEvent ? "Yes" : "No"}</span>
+    //           <div
+    //             className="grid grid-cols-2 gap-2 p-5
+    //         text-slate-600 bg-slate-100 rounded-lg m-5"
+    //           >
+    //             <span className="font-primary font-bold text-slate-800">
+    //               Solo Event
+    //             </span>
+    //             <span>{event?.isSoloEvent ? "Yes" : "No"}</span>
 
-                <span className="font-primary font-bold text-slate-800 ">
-                  Max Team Size
-                </span>
-                <span>{event?.maxTeamSize}</span>
+    //             <span className="font-primary font-bold text-slate-800 ">
+    //               Max Team Size
+    //             </span>
+    //             <span>{event?.maxTeamSize}</span>
 
-                <span className="font-primary font-bold text-slate-800">
-                  Min Team Size
-                </span>
-                <span>{event?.minTeamSize}</span>
-              </div>
+    //             <span className="font-primary font-bold text-slate-800">
+    //               Min Team Size
+    //             </span>
+    //             <span>{event?.minTeamSize}</span>
+    //           </div>
 
-              <p>Team Leader</p>
-              <input
-                className="shadow px-4 py-2 rounded focus:outline-none bg-slate-100 my-2"
-                value={userTscId}
-                readOnly
-              />
+    //           <p>Team Leader</p>
+    //           <input
+    //             className="shadow px-4 py-2 rounded focus:outline-none bg-slate-100 my-2"
+    //             value={userTscId}
+    //             readOnly
+    //           />
 
-              <p>Description</p>
-              <textarea
-                className="shadow  px-4 py-2 resize rounded-md focus:outline-none rounded bg-slate-100"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-              />
+    //           <p>Description</p>
+    //           <textarea
+    //             className="shadow  px-4 py-2 resize rounded-md focus:outline-none rounded bg-slate-100"
+    //             value={description}
+    //             onChange={(e) => setDescription(e.target.value)}
+    //           />
 
-              {event && event?.minTeamSize > 1 && <p>Team Members</p>}
+    //           {event && event?.minTeamSize > 1 && <p>Team Members</p>}
 
-              {participants.map((_, i) => (
-                <span key={i} className="space-x-2 relative">
-                  <input
-                    className="shadow px-4 py-2 rounded focus:outline-none bg-slate-100 my-2"
-                    placeholder="Enter TSC ID of member"
-                    onChange={(e) => setParticipantData(i, e.target.value)}
-                  />
-                  <button
-                    onClick={() => removeParticipant(i)}
-                    className="transition ease-in-out delay-15 absolute hover:scale-110 top-4"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      fill="#666"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                      <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                    </svg>
-                  </button>
-                </span>
-              ))}
+    //           {participants.map((_, i) => (
+    //             <span key={i} className="space-x-2 relative">
+    //               <input
+    //                 className="shadow px-4 py-2 rounded focus:outline-none bg-slate-100 my-2"
+    //                 placeholder="Enter TSC ID of member"
+    //                 onChange={(e) => setParticipantData(i, e.target.value)}
+    //               />
+    //               <button
+    //                 onClick={() => removeParticipant(i)}
+    //                 className="transition ease-in-out delay-15 absolute hover:scale-110 top-4"
+    //               >
+    //                 <svg
+    //                   xmlns="http://www.w3.org/2000/svg"
+    //                   width="24"
+    //                   height="24"
+    //                   fill="#666"
+    //                   viewBox="0 0 16 16"
+    //                 >
+    //                   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+    //                   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+    //                 </svg>
+    //               </button>
+    //             </span>
+    //           ))}
 
-              <div className="w-full flex space-x-2 justify-center">
-                {event && event?.minTeamSize > 1 && (
-                  <SecondaryButton
-                    text="Add more"
-                    onClick={addMoreParticipant}
-                  />
-                )}
-                <PrimaryButton text="Register now" onClick={handleRegister} />
-              </div>
-            </>
-          ) : (
-            <>
-              <div>
-                <h1 className="font-primary text-3xl text-center rounded-lg shadow-lg ">
-                  ₹400 ticket-
-                </h1>
-                <ul className="list-disc m-5">
-                  <li className=" hover:bg-blue-50 ">
-                    Access to participate and compete in all 36 competitions
-                    organized in 'Technika'
-                  </li>
+    //           <div className="w-full flex space-x-2 justify-center">
+    //             {event && event?.minTeamSize > 1 && (
+    //               <SecondaryButton
+    //                 text="Add more"
+    //                 onClick={addMoreParticipant}
+    //               />
+    //             )}
+    //             <PrimaryButton text="Register now" onClick={handleRegister} />
+    //           </div>
+    //         </>
+    //       ) : (
+    //         <>
+    //           <div>
+    //             <h1 className="font-primary text-3xl text-center rounded-lg shadow-lg ">
+    //               ₹400 ticket-
+    //             </h1>
+    //             <ul className="list-disc m-5">
+    //               <li className=" hover:bg-blue-50 ">
+    //                 Access to participate and compete in all 36 competitions
+    //                 organized in 'Technika'
+    //               </li>
 
-                  <li className=" hover:bg-blue-50 ">
-                    Free pass to celebrity night
-                  </li>
+    //               <li className=" hover:bg-blue-50 ">
+    //                 Free pass to celebrity night
+    //               </li>
 
-                  <li className=" hover:bg-blue-50 ">Free pass to fun fair </li>
+    //               <li className=" hover:bg-blue-50 ">Free pass to fun fair </li>
 
-                  <li className="hover:bg-blue-50 ">Free pass to EDM Night</li>
-                </ul>
+    //               <li className="hover:bg-blue-50 ">Free pass to EDM Night</li>
+    //             </ul>
 
-                <h1 className="font-primary text-3xl text-center rounded-lg shadow-lg ">
-                  ₹2000 ticket-
-                </h1>
+    //             <h1 className="font-primary text-3xl text-center rounded-lg shadow-lg ">
+    //               ₹2000 ticket-
+    //             </h1>
 
-                <ul className="list-disc m-5">
-                  <li className="hover:bg-blue-50 ">
-                    3 days stay in HBTU hostels, breakfast, dinner, and snacks
-                    included
-                  </li>
+    //             <ul className="list-disc m-5">
+    //               <li className="hover:bg-blue-50 ">
+    //                 3 days stay in HBTU hostels, breakfast, dinner, and snacks
+    //                 included
+    //               </li>
 
-                  <li className="hover:bg-blue-50 ">
-                    Access to participate and compete in all 36 competitions
-                    organized in 'Technika'
-                  </li>
+    //               <li className="hover:bg-blue-50 ">
+    //                 Access to participate and compete in all 36 competitions
+    //                 organized in 'Technika'
+    //               </li>
 
-                  <li className="hover:bg-blue-50">
-                    Free pass to celebrity night
-                  </li>
+    //               <li className="hover:bg-blue-50">
+    //                 Free pass to celebrity night
+    //               </li>
 
-                  <li className="hover:bg-blue-50">Free pass to fun fair</li>
+    //               <li className="hover:bg-blue-50">Free pass to fun fair</li>
 
-                  <li className="hover:bg-blue-50">Free pass to EDM Night</li>
-                </ul>
-              </div>
-              <span className="flex justify-center space-x-2 items-center w-full">
-                <span>Need accomations?</span>
-                <input
-                  type="checkbox"
-                  onChange={(e) =>
-                    setAccomation(e.target.value == "on" ? true : false)
-                  }
-                />
-              </span>
-              <PrimaryButton
-                text="Pay for all events now"
-                onClick={handlePayment}
-              />
-            </>
-          )}
-        </>
-      )}
-    </div>
+    //               <li className="hover:bg-blue-50">Free pass to EDM Night</li>
+    //             </ul>
+    //           </div>
+    //           <span className="flex justify-center space-x-2 items-center w-full">
+    //             <span>Need accomations?</span>
+    //             <input
+    //               type="checkbox"
+    //               onChange={(e) =>
+    //                 setAccomation(e.target.value == "on" ? true : false)
+    //               }
+    //             />
+    //           </span>
+    //           <PrimaryButton
+    //             text="Pay for all events now"
+    //             onClick={handlePayment}
+    //           />
+    //         </>
+    //       )}
+    //     </>
+    //   )}
+    // </div>
+    <div></div>
   );
 }
